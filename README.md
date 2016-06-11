12cacd9182daaf760f656739e8c5410ef419aa3c
Maximilian Weber <weber.maximilian@googlemail.com>
2016-06-11 10:41:49 +0200

    Extracted sv.system.core from sv/system.

---
+++ README.md
@@ -0,0 +1,14 @@
+# system.core
+
+System is an approach to implement component-based systems.
+
+## Usage
+
+See https://github.com/SimpleValue/sv.system
+
+## License
+
+Copyright © 2016 Max Weber (SimpleValue)
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject sv/system.core "0.1.0-SNAPSHOT"
+  :description "System is an approach to implement component-based systems"
+  :url "https://github.com/SimpleValue/sv.system"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [com.stuartsierra/dependency "0.2.0"]])
